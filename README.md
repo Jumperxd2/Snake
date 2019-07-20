5e47f1197fc3ea2aec0cfc63b03a5335a13aed44
Eli Gaitley <ceg8b4@mst.edu>
2019-07-20 16:30:38 -0500

    Completed part 2 of the tutorial.

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 This repository contains code for a simple snake game in JavaScript.
 
 [Tutorial Part 1](https://hjnilsson.com/2018/03/31/snake-in-pure-html-part-1/)
+[Tutorial Part 2](https://hjnilsson.com/2018/04/19/snake-in-pure-html-part-2/)
--- src/js/snake.js
+++ src/js/snake.js
@@ -7,16 +7,70 @@ var snakeY
 var snakeDirection
 var snakeLength
 
-function start () {
+function wallHit () {
+  return snakeX < 0 || snakeY < 0 || snakeX >= boardWidth || snakeY >= boardHeight
+}
+
+function keyPress (event) {
+  switch (event.key) {
+    case 'ArrowUp': snakeDirection = 'Up'; break
+    case 'ArrowDown': snakeDirection = 'Down'; break
+    case 'ArrowLeft': snakeDirection = 'Left'; break
+    case 'ArrowRight': snakeDirection = 'Right'; break
+    default: return
+  } // End Switch
+
+  event.preventDefault()
+} // End keyPress
+
+function gameLoop () {
+  switch (snakeDirection) {
+    case 'Up': snakeY--; break
+    case 'Down': snakeY++; break
+    case 'Left': snakeX--; break
+    case 'Right': snakeX++; break
+  } // End Switch
+
+  if (wallHit()) {
+    startGame()
+  }
+
+  board[snakeY][snakeX].snake = snakeLength
+
+  for (let y = 0; y < boardHeight; y++) {
+    for (let x = 0; x < boardWidth; x++) {
+      const cell = board[y][x]
+
+      if (cell.snake > 0) {
+        cell.element.className = 'snake'
+        cell.snake--
+      } else {
+        cell.element.className = ''
+      } // End if
+    } // End for
+  } // End for
+
+  setTimeout(gameLoop, 1000 / snakeLength)
+} // End gameLoop
+
+function startGame () {
+  for (let y = 0; y < boardHeight; y++) {
+    for (let x = 0; x < boardWidth; x++) {
+      board[y][x].snake = 0
+    } // End for
+  } // End for
+
   snakeX = Math.floor(boardWidth / 2)
   snakeY = Math.floor(boardHeight / 2)
   snakeLength = 5
   snakeDirection = 'Up'
 
-  board[snakeY][snakeX].snake = 1
-} // End start
+  board[snakeY][snakeX].snake = snakeLength
 
-function createBoard () {
+  gameLoop()
+} // End startGame
+
+function initGame () {
   const boardDiv = document.getElementById('board')
 
   for (let y = 0; y < boardHeight; y++) {
@@ -35,9 +89,6 @@ function createBoard () {
 
     board.push(row)
   } // End for
-} // End createBoard
 
-function init () {
-  createBoard()
-  start()
-} // End init
+  startGame()
+} // End initGame
